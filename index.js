95b78e4734479c1f1e751c3f2c26c085e22a77a8
meghats123 <meghats39032@gmail.com>
2021-02-04 22:15:11 +0530

    CRUD operations completed

--- index.js
+++ index.js
@@ -20,6 +20,76 @@ app.post('/empadd',async(req,res)=>{
     }
 
 })
+app.get("/viewall",async(req,res)=>{
+    try{
+        var result=await empModel.find().exec();
+        res.json(result);
+    }
+    catch(error){
+        res.status(500).send(error)
+    }
+})
+app.post("/search",async(req,res)=>{
+    try{
+        empModel.find(req.body,(error,data)=>{
+            if(error){
+                throw error;
+            }
+            else{
+                res.json(data)
+            }
+        })
+    }
+    catch(error){
+        res.status(500).send(error)
+    }
+})
+app.post("/delete",async(req,res)=>{
+    try{
+        empModel.findByIdAndDelete(req.body.id,(error,data)=>{
+            if(error){
+                res.send(error)
+            }
+            else{
+                res.json({'status':'success'})
+            }
+            
+        })
+    }
+    catch(error){
+        res.status(500).send(error)
+    }
+})
+app.post("/update",async(req,res)=>{
+    try{
+        empModel.findByIdAndUpdate(req.body.id,
+            {
+                empname:req.body.empname,
+                address:req.body.address,
+                phoneno:req.body.phoneno,
+                designation:req.body.designation,
+                email:req.body.email,
+                salary:req.body.salary,
+                companyname:req.body.companyname,
+                empcode:req.body.empcode,
+
+            },(error,data)=>{
+                if(error){
+                    throw error
+                }
+                else{
+                    res.json({'status':'success'})
+                    
+                }
+            }
+            )
+
+    }
+    catch(error){
+        res.status(500).send(error)
+    }
+    
+})
 app.listen(process.env.PORT || 3000,function(){
     console.log('Your node js server is running at http://localhost:3000')
 })
